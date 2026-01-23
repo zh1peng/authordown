4ee4149ef14aa1af042dc986ba0730be9b1a0237
zh1peng <frueddy@gmail.com>
2026-01-23 14:14:03 +0800

    rm app.R

--- app.R
+++
@@ -1,7 +0,0 @@
-library(shiny)
-
-app_dir <- file.path("inst", "shiny-app")
-ui <- source(file.path(app_dir, "ui.R"), local = TRUE)$value
-server <- source(file.path(app_dir, "server.R"), local = TRUE)$value
-
-shinyApp(ui = ui, server = server)
